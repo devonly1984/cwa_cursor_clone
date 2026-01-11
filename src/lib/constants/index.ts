658f1f9580011fd741155855f3025a02407bcf49
John Nobody <devonly1984@hotmail.com>
2026-01-10 22:10:50 -0600

    First Commit

---
+++ src/lib/constants/index.ts
@@ -0,0 +1,12 @@
+import { IBM_Plex_Mono, Inter } from "next/font/google";
+
+export const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const plexMono = IBM_Plex_Mono({
+  variable: "--font-plex-mono",
+  subsets: ["latin"],
+  weight: ['400','500','600','700']
+});
